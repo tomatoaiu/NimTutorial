d812f650359ef15651541d7d3f657ce33a009eb2
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-06-29 23:16:17 +0900

    feat: loop

---
+++ loop.nim
@@ -0,0 +1,17 @@
+echo "For: "
+for i in countup(1, 3):
+  echo i
+
+echo "While: "
+var i = 1
+while i <= 3:
+  echo i
+  inc(i)
+
+echo "For 1..i: "
+for index in 1..i:
+  echo index
+
+echo "For 1..<i: "
+for index in 1..<i:
+  echo index
