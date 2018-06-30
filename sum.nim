3dfd36c259944c8967f99edb40fcef9f72667c66
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-06-30 20:29:30 +0900

    feat: added the parameters

--- sum.nim
+++ sum.nim
@@ -2,4 +2,14 @@ proc sum(x: int, y: int): int =
   x + y
 
 echo sum(1, 2)
-echo sum(3, 4)
+echo sum(x = 3, y = 4)
+
+var
+  z: int
+
+proc sum(x, y: int, z: var int) =
+  z = x + y
+
+sum(5, 6, z)
+
+echo z
